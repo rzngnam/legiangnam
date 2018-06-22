0431bbbb2064c76a1c43e26cd43b331ee6571c9d
rangnam <gianggnamm1402@gmail.com>
2018-06-23 00:01:03 +0700

    new commit (playable)

--- lesson6/snake.py
+++ lesson6/snake.py
@@ -1,17 +1,16 @@
 import pygame
 import random
-from pygame.locals import *
+
 
 width = 800
 height = 600
 
 #  tạo khung hiển thị game
 display_surf = pygame.display.set_mode((width, height))
-pygame.display.set_caption(" Snake ")
 
 red = (250, 0, 0)
 green = (0, 255, 0)
-direction = [20, -20]
+black = (0, 0, 0)
 
 fps = 20  # frame per second
 fps_clock = pygame.time.Clock()
@@ -23,82 +22,138 @@ class Apple:
         self.y = y
 
     def draw(self):
-        pygame.draw.rect(display_surf, red, (self.x, self.y, random.randrange(20, 600, 20),
-                                             random.randrange(20, 600, 20)))
-
-    def update(self, snake):
-        if (self.x == snake.x) and (self.y == snake.y):
-            self.x = random.randrange(20, 600, 20)
-            self.y = random.randrange(20, 600, 20)
+        pygame.draw.rect(display_surf, red, (self.x, self.y, 20, 20))
 
 
 class Snake:
-    body_x = [20, 40, 60]   # body_x[0] và body_y[0] là head của rắn
-    body_y = [20, 20, 20]
+    body_x = []   # body_x[0] và body_y[0] là head của rắn
+    body_y = []
 
-    def __init__(self, w, h, tail, speed):
+    def __init__(self, w, h, tail, speed, player_move, old_dir):
         self.w = w
         self.h = h
         self.tail = tail
         self.speed = speed
+        self.old_dir = old_dir
+        self.player_move = player_move
+        for i in range(self.tail):
+            self.body_x.append(20)
+            self.body_y.append(20)
 
-    def update(self):
-        for i in range(self.tail - 1, 0, -1):
-            self.body_x[i] = self.body_x[i - 1]
-            self.body_y[i] = self.body_y[i - 1]
+    def update(self, player_move, apple):
 
-        if direction == 1:
-            self.move_right()
-        elif direction == 2:
-            self.move_left()
-        elif direction == 3:
-            self.move_up()
-        elif direction == 4:
-            self.move_down()
+            for i in range(self.tail - 1, 0, -1):
+                self.body_x[i] = self.body_x[i - 1]
+                self.body_y[i] = self.body_y[i - 1]
+
+            if player_move == 1 and self.old_dir != 'left':
+                self.move_right()
+                self.old_dir = 'right'
+
+            if player_move == 2 and self.old_dir != 'right':
+                self.move_left()
+                self.old_dir = 'left'
+
+            if player_move == 3 and self.old_dir != 'down':
+                self.move_up()
+                self.old_dir = 'up'
+
+            if player_move == 4 and self.old_dir != 'up':
+                self.move_down()
+                self.old_dir = 'down'
+
+            if self.body_x[0] == apple.x and self.body_y[0] == apple.y:
+                self.tail += 1
+                self.body_x.append(self.body_x[-1])  # Táo bị ăn => body dài ra
+                self.body_y.append(self.body_y[-1])
+                apple.x = random.randrange(20, 600, 20)
+                apple.y = random.randrange(20, 600, 20)
 
     def move_right(self):
-        self.body_x[0] += direction[0]*self.speed
+        self.body_x[0] += 20*self.speed
 
     def move_left(self):
-        self.body_x[0] += direction[1]*self.speed
+        self.body_x[0] += -20*self.speed
 
     def move_up(self):
-        self.body_y[0] += direction[1]*self.speed
+        self.body_y[0] += -20*self.speed
 
     def move_down(self):
-        self.body_y[0] += direction[0]*self.speed
+        self.body_y[0] += 20*self.speed
 
-    def eat(self, apple):
-        if (self.body_x[0] == apple.x) and (self.body_y[0] == apple.y):
-            self.tail += 1
-            self.body_x.append(self.body_x[-1])  # Táo bị ăn => body dài ra
-            self.body_y.append(self.body_y[-1])
+    # def eat(self, apple):
+    #
+    #         return True
+    #     else:
+    #         return False
 
     def death(self):
         for i in range(self.tail):
             if (self.body_x[0] == self.body_x[i]) and (self.body_y[0] == self.body_y[i]):
-                self.tail = 1
-                self.body_x = [20, 40, 60]   # reset the game
-                self.body_y = [20, 20, 20]
+                return True
+            else:
+                return False
 
     def hit_celling(self):
-        if self.body_y[0] == 0:
+        if self.body_y[0] < 0:
             return True
         else:
             return False
 
     def hit_floor(self):
-        if self.body_y[0] == height - self.h:
+        if self.body_y[0] == height:
             return True
         else:
             return False
 
     def hit_edge(self):
-        if (self.body_x[0] == 0) or (self.body_x == width - self.w):
+        if (self.body_x[0] < 0) or (self.body_x[0] > width):
             return True
         else:
             return False
 
     def draw(self):
+
         for i in range(self.tail):
-            pygame.draw.rect(display_surf, green, (self.body_x[i], self.body_y[i], 20, 20))
+            if i == 0:
+                pygame.draw.rect(display_surf, (255, 242, 0), (self.body_x[0], self.body_y[0], 20, 20))
+            else:
+                pygame.draw.rect(display_surf, green, (self.body_x[i], self.body_y[i], 20, 20))
+
+
+def main():
+    pygame.init()
+    running = True
+    pygame.display.set_caption("Snake")
+    player = Snake(20, 20, 4, 1, 1, 1)
+    food = Apple(560, 400)
+    player_move = 0
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    player_move = 1
+                elif event.key == pygame.K_LEFT:
+                    player_move = 2
+                elif event.key == pygame.K_UP:
+                    player_move = 3
+                elif event.key == pygame.K_DOWN:
+                    player_move = 4
+        display_surf.fill(black)
+        player.draw()
+        food.draw()
+        if player.hit_floor():
+            exit()
+        if player.hit_celling():
+            exit()
+        if player.hit_edge():
+            exit()
+        player.update(player_move, food)
+        fps_clock.tick(10)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
